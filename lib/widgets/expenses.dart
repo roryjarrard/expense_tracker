390c9cafb69c7a2728464cbd01f7c5713d3f5bf5
Rory Jarrard <rjarrard@gmail.com>
2024-05-16 10:02:31 -0600

    restructure

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/components/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
